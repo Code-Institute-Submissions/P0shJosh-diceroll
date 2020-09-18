f1f01c96d88247666d71dd1cf813e441f98e3490
P0shJosh <jphodge.96@gmail.com>
2020-09-18 13:43:02 +0100

    FEAT: Replaced footer with modals and began filling in content/styling

--- assets/js/eoedice.js
+++ assets/js/eoedice.js
@@ -175,4 +175,14 @@ window.addEventListener("load", () => {
       $("#results").hide();
     });
     
+    $('.input2').each(function(){
+      $(this).on('blur', function(){
+          if($(this).val().trim() != "") {
+              $(this).addClass('has-val');
+          }
+          else {
+              $(this).removeClass('has-val');
+          }
+      })    
+  })
   });
